3e9b058685a9666a8c5d013053a0c8b36711457a
LPXTT <33859603+LPXTT@users.noreply.github.com>
2022-12-23 10:44:09 +1100

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 PYTHONPATH="$(dirname $0)":$PYTHONPATH
 
-srun -p ai4science -n1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=5 --quotatype spot \
+#srun -p ai4science -n1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=5 --quotatype spot \
 python tracking/train.py --script simtrack --config baseline_got10k_only --save_dir . --mode multiple --nproc_per_node 8
